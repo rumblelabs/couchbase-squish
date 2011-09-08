a3c8de7a012c5167dbfa195c7127de1b7bc3201a
David Rice <me@davidjrice.co.uk>
2011-09-08 18:36:31 +0100

    Github ftw

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CouchbaseTinyurl::Application.config.secret_token = '555283902349a0673b9382aa1fa4c2db599b6c692046bd83859619f525d268c2973fb8a4000bece80c309833ea28bfe15851d1a21cdb37c2ca5327da97ef0cc5'
