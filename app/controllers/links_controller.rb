78b02b0eae619d97c24f6d8ce649589285935e87
Will McNeilly <wmcneilly@rumblelabs.com>
2011-09-15 17:54:49 +0100

    Filter link columns by type t:35m

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,9 +1,10 @@
 class LinksController < ApplicationController
 
   def index
-    @popular = Link.popular
-    @recent = Link.recent
-    @my_links = Link.by_session_id(session[:session_id])
+    @filter = params[:filter] ||= "my_links"
+    @popular = Link.popular if params[:filter] == "popular"
+    @recent = Link.recent if params[:filter] == "recent"
+    @my_links = Link.by_session_id(session[:session_id]) if params[:filter] == "my_links"
     @link = Link.new
   end
 
